9c0f83e1102332416a1016f129c043a0a71be819
Fabián Ezequiel Gallina <fgallina@cuca>
2010-06-11 17:46:41 -0300

    Better selenium client initialization.
    The default browser url had a wrong default. now it uses SELTEST_HOST and SELTEST_PORT.

--- seltest/config.py
+++ seltest/config.py
@@ -7,10 +7,10 @@ PORT = getattr(settings, 'SELTEST_PORT', 8001)
 
 SELENIUM_HOST = getattr(settings, 'SELTEST_SELENIUM_HOST', '127.0.0.1')
 SELENIUM_PORT = getattr(settings, 'SELTEST_SELENIUM_PORT', 4444)
-BROWSER_COMMAND = getattr(settings, 'SELTEST_BROWSER_COMMAND', "firefox")
+BROWSER_COMMAND = getattr(settings, 'SELTEST_BROWSER_COMMAND', "*firefox")
 DEFAULT_URL = getattr(
     settings, 'SELTEST_DEFAULT_URL',
-    "http://%s:%s" % (SELENIUM_HOST, SELENIUM_PORT)
+    "http://%s:%s" % (HOST, PORT)
 )
 
 SELENIUM_SERVER_PATH = getattr(settings, 'SELTEST_SELENIUM_SERVER_PATH', '/opt/selenium-server.jar')
